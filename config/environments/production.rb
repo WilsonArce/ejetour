cd22e161c226a487fd93ba3bdeed7b9771ba04e2
Wilson Arce <wdarcedev@gmail.com>
2017-05-02 18:13:43 -0500

    config prod mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ejetourcafe_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  #mail
+  #Mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: 'https://ejetourcafe.herokuapp.com',
+    domain: 'gmail.com',
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "wdarceq@gmail.com",
     password: "gm123456"
   }
-  config.action_mailer.default_url_options = { host: 'https://ejetourcafe.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'ejetourcafe.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
