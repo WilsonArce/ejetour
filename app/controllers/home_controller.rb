5f057599725227052bfea295d1ed2e1c4cebfdf6
Wilson Arce <wdarcedev@gmail.com>
2017-05-27 12:01:25 -0500

    main page view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
+  	@last_articles ||= []
+  	@categories = Category.all
+  	@categories.each do |category|
+  		@articles = category.articles
+  		@last_articles.push(@articles.last)
+  	end
   end
 end
